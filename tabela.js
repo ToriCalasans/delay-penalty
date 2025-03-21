dab7a3d9666f19bde859c973d956f4d67744e97c
Tori Calasans <56488155+ToriCalasans@users.noreply.github.com>
2025-03-21 18:56:38 +0000

    feito alteração para tabela apresentar nome do meliante

--- tabela.js
+++ tabela.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const nameUSer = localStorage.getItem("name");
+  const nameUser = localStorage.getItem("name");
   //Pega o nome salvo
 
   if (nameUser) {
-    document.getElementById(nameUser).innerText = `Tabela de ${nameUser}`;
+    document.getElementById("nameUser").innerText = `Tabela de ${nameUser}`;
+
     // Mostra o nome da tabela
   }
 });
+
+document.addEventListener
